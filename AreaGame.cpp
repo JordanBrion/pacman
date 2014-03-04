f340f8e2300f5699c70d7891f9a6fbb705ac6033
Jordan Brion <jrd.brion@gmail.com>
2014-03-04 23:32:39 +0100

    Const file

---
+++ AreaGame.cpp
@@ -0,0 +1,5 @@
+#include "AreaGame.h"
+
+AreaGame::AreaGame()
+{
+}
---
+++ AreaGame.h
@@ -0,0 +1,10 @@
+#ifndef AREAGAME_H
+#define AREAGAME_H
+
+class AreaGame
+{
+public:
+    AreaGame();
+};
+
+#endif // AREAGAME_H
