0406664f524209c418ffd4968c352a677f17c41b
Jordan Brion <jrd.brion@gmail.com>
2014-03-06 00:13:20 +0100

    Personnage class creation

--- Personnage.cpp
+++ Personnage.cpp
@@ -1,2 +1,9 @@
 #include "Personnage.h"
 
+using namespace std;
+
+Personnage::Personnage(map<string, int> src, map<string, int> dest, SDL_Renderer* const& renderer, SDL_Surface* const& sprite)
+    : InteractiveElement(src, dest, renderer, sprite) {
+
+
+}
--- Personnage.h
+++ Personnage.h
@@ -6,19 +6,22 @@
 class Personnage: public InteractiveElement {
 
 protected:
-    int _vitesse;
-    bool _dead;
-    std::map<std::string, int> _positionForRespawn;
-    std::map<std::string, bool> _directionsPossible;
-    std::map<std::string, bool> _directionsPossibleAnticipated;
-
-protected:
+    Personnage(std::map<std::string, int>, std::map<std::string, int>, SDL_Renderer* const&, SDL_Surface* const&);
     virtual void move () = 0;
     virtual void stopMove () = 0;
     virtual void checkCollision () = 0;
     virtual void deadAnimation () = 0;
     virtual void teleportation () = 0;
     virtual void calculateDirection ( std::map<std::string, bool> ) = 0;
+
+protected:
+    int _vitesse;
+    bool _dead;
+    int _spriteFlag;
+    std::vector<std::map<std::string, int> > _spriteCoord;
+    std::map<std::string, int> _positionForRespawn;
+    std::map<std::string, bool> _directionsPossible;
+    std::map<std::string, bool> _directionsPossibleAnticipated;
 
 };
 
