7cc2c0e24e5518ee8be1c751795257623ac626a3
Jordan Brion <jrd.brion@gmail.com>
2014-04-07 22:14:08 +0200

    Class for top window area

---
+++ AreaTop.cpp
@@ -0,0 +1,11 @@
+#include "AreaTop.h"
+
+AreaTop::AreaTop ( SDL_Renderer* const& pRenderer, TTF_Font* const& pFont ) {}
+
+void AreaTop::drawArea ( SDL_Renderer* const pRenderer, TTF_Font* const& pFont ) {}
+
+void AreaTop::drawHighScore ( int const& highscore ) {}
+
+void AreaTop::drawPlayerOneScore ( int const& score ) {}
+
+void AreaTop::drawPlayerTwoScore ( int const& score ) {}
---
+++ AreaTop.h
@@ -0,0 +1,21 @@
+#ifndef AREATOP_H
+#define AREATOP_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include "Area.h"
+#include "Const.h"
+
+class AreaTop: public Area {
+
+public:
+    AreaTop ( SDL_Renderer* const& pRenderer, TTF_Font* const& pFont );
+    void drawArea ( SDL_Renderer* const pRenderer, TTF_Font* const& pFont );
+    void drawHighScore ( int const& highscore );
+    void drawPlayerOneScore ( int const& score );
+    void drawPlayerTwoScore ( int const& score );
+
+};
+
+#endif
