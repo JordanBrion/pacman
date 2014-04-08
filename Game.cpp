1570a2128c6638d0a3ab0eb69a114435572ed20d
Jordan Brion <jrd.brion@gmail.com>
2014-04-09 00:11:13 +0200

    Getters and setters implementations

--- Game.cpp
+++ Game.cpp
@@ -2,43 +2,100 @@
 
 using namespace std;
 
-Game::Game() : _comboSuperPower(0), _score(0), _paused(false) {
+Game::Game() : _comboSuperPower(0), _scoreP1(0), _scoreP2(0), _highScore(0), _paused(false) {
 }
 
 Game::~Game() {
 }
 
-int Game::getNbrLifes() const {
+int Game::getBubblesNbrInit() const {
+
+    return _bubblesNbrInit;
+
 }
 
-void Game::incNbrLifes() {
+void Game::setBubblesNbrInit(int const& nbr) {
+
+    _bubblesNbrInit = nbr;
+
 }
 
-void Game::decNbrLifes() {
+int Game::getBubblesNbr() const {
+
+    return _bubblesNbr;
+
+}
+
+void Game::setBubblesNbr(int const& nbr) {
+
+    _bubblesNbr = nbr;
+
+}
+
+int Game::getLifesNbr() const {
+
+    return _lifesNbr;
+
+}
+
+void Game::incLifesNbr() {
+
+    _lifesNbr++;
+
+}
+
+void Game::decLifesNbr() {
+
+    _lifesNbr--;
+
 }
 
 int Game::getScoreP1() const {
+
+    return _scoreP1;
+
 }
 
-void Game::setScoreP1() {
+void Game::setScoreP1(int const& score) {
+
+    _scoreP1 = score;
+
 }
 
 int Game::getScoreP2() const {
+
+    return _scoreP2;
+
 }
 
-void Game::setScoreP2() {
+void Game::setScoreP2(int const& score) {
+
+    _scoreP2 = score;
+
 }
 
 int Game::getHighScore() const {
+
+    return _highScore;
+
 }
 
-void Game::setHighScore() {
+void Game::setHighScore(int const& score) {
+
+    _highScore = score;
+
 }
 
 bool Game::isTwoPlayersGame() const {
+
+    return _twoPlayersGame;
+
 }
 
 void Game::setTwoPlayersGame(bool const& solo) {
+
+    _twoPlayersGame = solo;
+
 }
 
 
--- Game.h
+++ Game.h
@@ -8,25 +8,31 @@ class Game {
 public:
     Game();
     ~Game();
-    int getNbrLifes() const;
-    void incNbrLifes();
-    void decNbrLifes();
+    int getBubblesNbrInit() const;
+    void setBubblesNbrInit(int const& nbr);
+    int getBubblesNbr() const;
+    void setBubblesNbr(int const& nbr);
+    int getLifesNbr() const;
+    void incLifesNbr();
+    void decLifesNbr();
     int getScoreP1() const;
-    void setScoreP1();
+    void setScoreP1(int const& score);
     int getScoreP2() const;
-    void setScoreP2();
+    void setScoreP2(int const& score);
     int getHighScore() const;
-    void setHighScore();
+    void setHighScore(int const& score);
     bool isTwoPlayersGame() const;
     void setTwoPlayersGame(bool const& solo);
 
 private:
-    int _nbrBubbles;
-    int _nbrLifes;
+    int _bubblesNbrInit;
+    int _bubblesNbr;
+    int _lifesNbr;
     std::vector<int> _fruits;
     int _comboSuperPower;
-    int _score;
-    int _highscore;
+    int _scoreP1;
+    int _scoreP2;
+    int _highScore;
     bool _paused;
     bool _twoPlayersGame;
 
