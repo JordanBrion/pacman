0259c1946552982674dec7f5b35e65350121d7e9
Jordan Brion <jrd.brion@gmail.com>
2014-04-09 22:53:05 +0200

    Methods to render texts and scores

--- AreaTop.cpp
+++ AreaTop.cpp
@@ -1,18 +1,133 @@
 #include "AreaTop.h"
 
+using namespace std;
+
 AreaTop::AreaTop() {}
 
 void AreaTop::drawArea( SDL_Renderer* const pRenderer, TTF_Font* const& pFont,
                         int const& pHightScore, int const& pScoreP1, int const& pScoreP2 ) {
 
-    drawPlayerOneScore(pScoreP1);
-    drawHighScore(pHightScore);
-    drawPlayerTwoScore(pScoreP2);
+    drawHighScore(pRenderer, pFont, pHightScore);
+    drawPlayerOneScore(pRenderer, pFont, pScoreP1);
+    drawPlayerTwoScore(pRenderer, pFont, pScoreP2);
 
 }
 
-void AreaTop::drawHighScore ( int const& highscore ) {}
+void AreaTop::drawHighScore( SDL_Renderer* const& pRenderer, TTF_Font* const& pFont, int const& pHighScore ) {
 
-void AreaTop::drawPlayerOneScore ( int const& score ) {}
+    // Render text "High Score"
+    drawText(pRenderer, pFont, "High Score", SDL_WHITE);
 
-void AreaTop::drawPlayerTwoScore ( int const& score ) {}
+    initPositionText(
+                150,
+                60,
+                375,
+                20);
+
+    SDL_RenderCopy( pRenderer, _texture, NULL, &_position );
+
+    // Render the High Score number
+    drawText(pRenderer, pFont, convertIntToChar(pHighScore), SDL_WHITE);
+
+    initPositionText(
+                20 + scoreTextureWidth(pHighScore),
+                20,
+                900 / 2 + 60 - scoreTextureWidth(pHighScore),
+                _position.y + _position.h - 10);
+
+    SDL_RenderCopy( pRenderer, _texture, NULL, &_position );
+
+}
+
+void AreaTop::drawPlayerOneScore( SDL_Renderer* const& pRenderer, TTF_Font* const& pFont, int const& pScore ) {
+
+    // Render text "1UP"
+    drawText(pRenderer, pFont, "1UP", SDL_WHITE);
+
+    initPositionText(
+                60,
+                40,
+                170,
+                25);
+
+    SDL_RenderCopy( pRenderer, _texture, NULL, &_position );
+
+    // Render the Player 1 Score number
+    drawText(pRenderer, pFont, convertIntToChar(pScore), SDL_WHITE);
+
+    initPositionText(
+                20 + scoreTextureWidth(pScore),
+                20,
+                _position.x + 40 - scoreTextureWidth(pScore),
+                _position.y + _position.h - 8);
+
+    SDL_RenderCopy( pRenderer, _texture, NULL, &_position );
+
+}
+
+void AreaTop::drawPlayerTwoScore( SDL_Renderer* const& pRenderer, TTF_Font* const& pFont, int const& pScore ) {
+
+    // Render text "2UP"
+    drawText(pRenderer, pFont, "2UP", SDL_WHITE);
+
+    initPositionText(
+                60,
+                40,
+                650,
+                25);
+
+    SDL_RenderCopy( pRenderer, _texture, NULL, &_position );
+
+    // Render the Player 2 Score number
+    drawText(pRenderer, pFont, convertIntToChar(pScore), SDL_WHITE);
+
+    initPositionText(
+                20 + scoreTextureWidth(pScore),
+                20,
+                _position.x + 40 - scoreTextureWidth(pScore),
+                _position.y + _position.h - 8);
+
+    SDL_RenderCopy( pRenderer, _texture, NULL, &_position );
+
+}
+
+int AreaTop::scoreTextureWidth(int pScore) {
+
+    int width = 0;
+
+    while( pScore > 9 ) {
+
+        pScore /= 10;
+        width += 20;
+
+    }
+
+    return width;
+
+}
+
+const char* AreaTop::convertIntToChar(int const& pScore) {
+
+    string s = to_string(pScore);
+    char const *c = s.c_str();
+
+    return c;
+
+}
+
+void AreaTop::initPositionText(int const& w, int const& h, int const& x, int const& y) {
+
+    _position.w = w;
+    _position.h = h;
+    _position.x = x;
+    _position.y = y;
+
+}
+
+void AreaTop::drawText(SDL_Renderer* const& pRenderer, TTF_Font* const& pFont, const char* pText, SDL_Color pColor) {
+
+    SDL_Surface* textSurface = TTF_RenderText_Solid( pFont, pText, pColor );
+    _texture = SDL_CreateTextureFromSurface( pRenderer, textSurface );
+    SDL_FreeSurface(textSurface);
+
+}
--- AreaTop.h
+++ AreaTop.h
@@ -4,6 +4,8 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
+#include <string>
+#include <map>
 #include "Area.h"
 #include "Const.h"
 
@@ -12,9 +14,13 @@ class AreaTop: public Area {
 public:
     AreaTop();
     void drawArea( SDL_Renderer* const pRenderer, TTF_Font* const& pFont, int const& pHightScore, int const& pScoreP1, int const& pScoreP2 );
-    void drawHighScore ( int const& highscore );
-    void drawPlayerOneScore ( int const& score );
-    void drawPlayerTwoScore ( int const& score );
+    void drawHighScore( SDL_Renderer* const& pRenderer, TTF_Font* const& pFont, int const& pHighScore );
+    void drawPlayerOneScore( SDL_Renderer* const& pRenderer, TTF_Font* const& pFont, int const& pScore );
+    void drawPlayerTwoScore( SDL_Renderer* const& pRenderer, TTF_Font* const& pFont, int const& pScore );
+    int scoreTextureWidth(int pScore);
+    const char* convertIntToChar(int const& pScore);
+    void initPositionText(int const& w, int const& h, int const& x, int const& y);
+    void drawText(SDL_Renderer* const& pRenderer, TTF_Font* const& pFont, const char* pText, SDL_Color pColor);
 
 };
 
