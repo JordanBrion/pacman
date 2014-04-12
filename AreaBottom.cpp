e9a9e45a72c64059b77d0702fdeb8089710e1209
Jordan Brion <jrd.brion@gmail.com>
2014-04-12 23:47:20 +0200

    Implementation of AreaBottom methods

--- AreaBottom.cpp
+++ AreaBottom.cpp
@@ -2,11 +2,65 @@
 
 using namespace std;
 
-AreaBottom::AreaBottom ( SDL_Renderer* const& pRenderer, SDL_Surface* const& pSpriteCharacters ) {}
+AreaBottom::AreaBottom() {}
 
-void AreaBottom::drawArea ( SDL_Renderer* const& pRenderer, SDL_Surface* const& pSpriteCharacters ) {}
+void AreaBottom::drawLifesPanel( SDL_Renderer* const& pRenderer, SDL_Surface* const& pSpriteCharacters, TTF_Font* const& pFont, int const& pLifesNbr ) {
 
-void AreaBottom::drawLifesPanel ( int const& lifesNbr ) {}
+    // Show lifes number image
+    _texture = SDL_CreateTextureFromSurface(pRenderer, pSpriteCharacters);
 
-void AreaBottom::drawFruitsPanel ( vector<int> fruits ) {}
+    _selection.w = 35;
+    _selection.h = 38;
+    _selection.x = 283;
+    _selection.y = 43;
+
+    _position.w = 30;
+    _position.h = 30;
+    _position.x = AREAGAME_MARGIN + 10;
+    _position.y = AREAGAME_HEIGHT;
+
+    SDL_RenderCopy( pRenderer, _texture, &_selection, &_position );
+
+    // Show lifes number
+    string s = to_string(pLifesNbr);
+    char const *c = s.c_str();
+    SDL_Surface* textSurface = TTF_RenderText_Solid( pFont, c, SDL_WHITE );
+    _texture = SDL_CreateTextureFromSurface(pRenderer, textSurface);
+    _position.x += 40;
+
+    SDL_RenderCopy( pRenderer, _texture, NULL, &_position );
+
+    SDL_FreeSurface( textSurface );
+
+}
+
+void AreaBottom::drawCollectiblesPanel( SDL_Renderer* const& pRenderer, SDL_Surface* const& pSpriteCharacters, TTF_Font* const& pFont, vector<Collectible*> pCollectibles ) {
+
+    _texture = SDL_CreateTextureFromSurface(pRenderer, pSpriteCharacters);
+
+    SDL_Surface* textSurface = NULL;
+
+    for( int i(0); i < pCollectibles.size(); i++ ) {
+
+        // Show collectible image
+        pCollectibles[i]->initPositionAreaBottom();
+        _selection = pCollectibles[i]->getSelection();
+        _position = pCollectibles[i]->getPosition();
+        pCollectibles[i]->show( pRenderer );
+
+        // Show collectible number
+        string s = to_string( pCollectibles[i]->getNbr() );
+        char const *c = s.c_str();
+        textSurface = TTF_RenderText_Solid( pFont, c, SDL_WHITE );
+        _texture = SDL_CreateTextureFromSurface(pRenderer, textSurface);
+        _position.x = pCollectibles[i]->getPosition().x + 30;
+
+        SDL_RenderCopy( pRenderer, _texture, NULL, &_position );
+
+    }
+
+    SDL_FreeSurface( textSurface );
+
+}
+
 
--- AreaBottom.h
+++ AreaBottom.h
@@ -3,17 +3,19 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include <vector>
+#include <string>
 #include "Area.h"
+#include "Collectible.h"
 #include "Const.h"
 
 class AreaBottom: public Area {
 
 public:
-    AreaBottom ( SDL_Renderer* const& pRenderer, SDL_Surface* const& pSpriteCharacters );
-    void drawArea ( SDL_Renderer* const& pRenderer, SDL_Surface* const& pSpriteCharacters );
-    void drawLifesPanel ( int const& lifesNbr );
-    void drawFruitsPanel ( std::vector<int> fruits );
+    AreaBottom();
+    void drawLifesPanel( SDL_Renderer* const& pRenderer, SDL_Surface* const& pSpriteCharacters, TTF_Font* const& pFont, int const& pLifesNbr );
+    void drawCollectiblesPanel( SDL_Renderer* const& pRenderer, SDL_Surface* const& pSpriteCharacters, TTF_Font* const& pFont, std::vector<Collectible*> pCollectibles );
 
 };
 
