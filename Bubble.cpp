b397fe6f8fa106a59077d2b864d2ba30f6d2676e
Jordan Brion <jrd.brion@gmail.com>
2014-04-18 16:42:07 +0200

    Change bubble diameter

--- Bubble.cpp
+++ Bubble.cpp
@@ -6,7 +6,7 @@ Bubble::Bubble( map<string, int> dest, int type, int x, int y, SDL_Renderer* con
     : InteractiveElement( dest, renderer, sprite ), _eaten( false ), _caseType( type ) {
 
     initRect(&_selection, 13, 13, 3, 183);
-    initRect(&_position, 20, 20, x, y);
+    initRect(&_position, 8, 8, x, y);
 
 }
 
