5dd64029719024165a38c812006809274ce1fd60
Jordan Brion <jrd.brion@gmail.com>
2014-09-02 14:53:10 +0200

    Doxygen : Fruit.h

--- src/Fruit.h
+++ src/Fruit.h
@@ -1,6 +1,14 @@
 #ifndef COLLECTIBLE_H
 #define COLLECTIBLE_H
 
+//!
+//! \file Fruit.h
+//! \brief Class of a fruit that can be picked up by pac-man
+//! \author Jordan Brion
+//! \version 0.1
+//! \date 2 september 2014
+//!
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <map>
@@ -8,23 +16,62 @@
 #include "Const.h"
 #include "InteractiveElement.h"
 
+//!
+//! \class Fruit
+//! \brief Class of a fruit that can be picked up by pac-man
+//!
 class Fruit : public InteractiveElement {
 
 public:
+    //!
+    //!  \brief Fruit constructor
+    //!  \param The name (type) of the fruit
+    //!  \param SDL_Renderer* of the window
+    //!  \param SDL_Surface* containing the sprite of all the characters (which containing also the fruit)
+    //!
     Fruit( int const& name, SDL_Renderer* const& renderer, SDL_Surface* const& sprite );
+    //!
+    //!  \brief Fruit destructor
+    //!
     ~Fruit();
+    //!
+    //!  \brief Getter for the type of fruit
+    //!  \return The type of the fruit
+    //!
     int getType() const;
+    //!
+    //!  \brief Setter to change the type of fruit
+    //!  \param The type of the fruit
+    //!
     void setType( int const& type );
+    //!
+    //!  \brief Getter for the number of this fruit
+    //!  \return The number of this fruit
+    //!
     int getNbr() const;
+    //!
+    //!  \brief Setter to change the number of this fruit
+    //!  \param The number of this fruit
+    //!
     void setNbr( int const& nbr );
+    //!
+    //!  \brief Getter for the number of points earned for this fruit
+    //!  \return The number of points earned for this fruit
+    //!
     int getPoints() const;
+    //!
+    //!  \brief Method to initialize the position where the fruit appears in the grid
+    //!
     void initPositionAreaGame();
+    //!
+    //!  \brief Method to initialize the position of the fruit in the area bottom
+    //!
     void initPositionAreaBottom();
 
 private:
-    int _type;
-    int _nbr;
-    int _points;
+    int _type;      /*!< The type of the fruit : CHERRY, STRAWBERRY, APRICOT, APPLE, MELON, GALXIAN_BOSS, BELL, KEY */
+    int _nbr;       /*!< The number of this fruit picked up */
+    int _points;    /*!< The number of points earned when the fruit is picked up */
 
 };
 
