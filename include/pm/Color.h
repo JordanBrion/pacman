b20c587b881b01a22ad2aeea64df55aec0579e77
Jordan Brion <jrd.brion@gmail.com>
2014-09-06 16:45:06 +0200

    New SDL colors

--- include/pm/Color.h
+++ include/pm/Color.h
@@ -4,5 +4,7 @@ namespace Color {
 
     static const SDL_Color SDL_WHITE = { 255, 255, 255, 0 };
     static const SDL_Color SDL_RED = { 255, 0, 0, 0 };
+    static const SDL_Color SDL_GREEN = { 0, 255, 0, 0 };
+    static const SDL_Color SDL_BLUE = { 0, 0, 255, 0 };
 
 }
