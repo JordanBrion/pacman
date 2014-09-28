e776d370a2bf072e7e681b5a461714b5dfd76e62
Jordan Brion <jrd.brion@gmail.com>
2014-09-28 13:12:15 +0200

    Blinking the power-pellets

--- src/PowerPellet.cpp
+++ src/PowerPellet.cpp
@@ -4,14 +4,23 @@
 
 using namespace std;
 
+/*
+ * STATIC ATTRIBUTES
+ *
+ * Static attributes because all the power-pellets has to use
+ * the same chronometers to blink at the same time
+ *
+ */
+Uint32 PowerPellet::blinkChrono = 0;
+Uint32 PowerPellet::betweenBlinkChrono = 0;
+
 PowerPellet::PowerPellet( map<string, int> dest,
                           int type,
                           int x,
                           int y,
                           SDL_Renderer* const& renderer,
                           SDL_Surface* const& sprite ) :
-    PacDot( dest, type, x, y, renderer, sprite ),
-    _blinkChrono( 0 ) {
+    PacDot( dest, type, x, y, renderer, sprite ) {
 
     initRect(&_position, 20, 20, x, y);
 
@@ -19,9 +28,45 @@ PowerPellet::PowerPellet( map<string, int> dest,
 
 PowerPellet::~PowerPellet() {}
 
-bool PowerPellet::blink() {}
+bool PowerPellet::blink() {
 
-void PowerPellet::resetBlinkChrono() {}
+    // Blink
+    if( SDL_GetTicks() - blinkChrono <= POWERPOWER_BLINK_DURATION ) {
+
+        return true;
+
+    }
+
+    // Don't blink
+    else if( SDL_GetTicks() - betweenBlinkChrono <= POWERPOWER_BLINK_BETWEEN_DURATION ) {
+
+        return false;
+
+    }
+
+}
+
+void PowerPellet::checkBlink() {
+
+    // Check if the blinkChrono is not over the duration
+    if( betweenBlinkChrono == 0
+            && SDL_GetTicks() - blinkChrono > POWERPOWER_BLINK_DURATION ) {
+
+        betweenBlinkChrono = SDL_GetTicks();
+        blinkChrono = 0;
+
+    }
+
+    // Check if the betweenBlinkChrono is not over the duration
+    else if( blinkChrono == 0
+             && SDL_GetTicks() - betweenBlinkChrono > POWERPOWER_BLINK_BETWEEN_DURATION ) {
+
+        blinkChrono = SDL_GetTicks();
+        betweenBlinkChrono = 0;
+
+    }
+
+}
 
 int PowerPellet::show( SDL_Renderer * const &renderer ) {
 
@@ -29,7 +74,12 @@ int PowerPellet::show( SDL_Renderer * const &renderer ) {
 
     if( !_eaten ) {
 
-        InteractiveElement::show( renderer );
+        PowerPellet::checkBlink();
+
+        // If the power-pellet has to blink, don't render it
+        if( PowerPellet::blink() ) {
+            InteractiveElement::show( renderer );
+        }
 
     }
     else {
--- src/PowerPellet.h
+++ src/PowerPellet.h
@@ -33,28 +33,28 @@ public:
     //!  \brief PowerPellet destructor
     //!
     ~PowerPellet();
-private:
-    //!
-    //! \brief Method to blink to power-pellet to the screen
-    //! \return True if the power-pellet has to blink. Otherwise, false
-    //!
-    bool blink();
-    //!
-    //! \brief Method to reset the blink chronometer
-    //!
-    void resetBlinkChrono();
-public:
     //!
     //!  \brief Method to render the power-pellet to the screen
     //!  \param renderer : SDL_Renderer* of the window
     //!  \return The points of the power-pellet if the power-pellet has not been eaten
     //!
     virtual int show( SDL_Renderer * const &renderer );
+    //!
+    //! \brief Static method to blink to power-pellet to the screen
+    //! \return True if the power-pellet has to blink. Otherwise, false
+    //!
+    static bool blink();
+    //!
+    //! \brief Static method to check and reset the chronometers
+    //!
+    static void checkBlink();
 
     /* ATTRIBUTES */
 private:
     static const Uint16 POWERPOWER_BLINK_DURATION = 1000;           /*!< Static attribute for the blink duration */
-    Uint32 _blinkChrono;                                            /*!< Blink chronometer */
+    static const Uint16 POWERPOWER_BLINK_BETWEEN_DURATION = 1000;   /*!< Static attribute for the duration between blink */
+    static Uint32 blinkChrono;                                      /*!< Static attribute for blink chronometer */
+    static Uint32 betweenBlinkChrono;                               /*!< Static attribute for between blink chronometer */
 
 };
 
