5683748db58c83790761a14f7935e48fd73534cd
Jordan Brion <jrd.brion@gmail.com>
2015-01-30 15:01:24 +0100

    Behavior when ghost is hunting Pac-Man

--- src/GhostAlgo/Behavior_Hunter.cpp
+++ src/GhostAlgo/Behavior_Hunter.cpp
@@ -14,6 +14,33 @@ Behavior_Hunter::Behavior_Hunter( Ghost* master,
 
 Behavior_Hunter::~Behavior_Hunter() {}
 
-void Behavior_Hunter::run() {}
+void Behavior_Hunter::run() {
+
+    // Update the ghost attributes
+    _ghost->updateAll();
+
+    // If the Pac-Man is not eatable
+    // => we change the behavior of the ghost
+    if( !_pacMan->isEatable() ) {
+
+        _ghost->setBehavior( new Behavior_Hunted( _ghost, _pacMan ) );
+
+    }
+
+    // Move the ghost
+    else if( _ghost->move() != -1 ) {
+
+        // After the move, detect if there is a collision
+        if( _ghost->checkCollision( _pacMan ) ) {
+
+            _pacMan->setDead( true );
+
+        }
+
+        _ghost->nextSprite();
+
+    }
+
+}
 
 Uint8 Behavior_Hunter::getState() const { return HUNTER; }
