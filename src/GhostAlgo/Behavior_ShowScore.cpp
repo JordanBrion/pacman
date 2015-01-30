e04e8032dd3045e68fd7c6691aae32950ed18753
Jordan Brion <jrd.brion@gmail.com>
2015-01-30 15:02:38 +0100

    Behavior when power-pellet score has to be rendered

--- src/GhostAlgo/Behavior_ShowScore.cpp
+++ src/GhostAlgo/Behavior_ShowScore.cpp
@@ -14,6 +14,24 @@ Behavior_ShowScore::Behavior_ShowScore( Ghost* master,
 
 Behavior_ShowScore::~Behavior_ShowScore() {}
 
-void Behavior_ShowScore::run() {}
+void Behavior_ShowScore::run() {
+
+    // Check if the power-pellet chrono is paused
+    if( _pacMan->getPowerPelletChrono()->isPaused()  ) {
+
+        // The ghost was eaten
+        // => trigger the GhostEatenScoreChrono
+        _pacMan->getGhostEatenScoreChrono()->start();
+
+        // The chrono is now running
+        // => wait for the end
+        SDL_WaitThread( _pacMan->getGhostEatenScoreChrono()->getThread(), 0 );
+
+    }
+
+    // The ghost was eaten => go to the Warpzone
+    _ghost->setBehavior( new Behavior_ReturnToWarpzone( _ghost, _pacMan ) );
+
+}
 
 Uint8 Behavior_ShowScore::getState() const { return SHOW_SCORE; }
